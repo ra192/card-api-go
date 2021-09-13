5a8025b0d5114a509e264e3c8095cd5df654458e
ra192 <yakovlev.roman@gmail.com>
2021-09-13 21:27:53 +0300

    change sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO public.merchants (id, name, secret) VALUES (1, 'Internal', 'C5X+rivjAkiczaDqkDwMKAVxN+cSGrudaGCRhWZeVos=');
-INSERT INTO public.merchants (id, name, secret) VALUES (2, 'Wayne Enterprise', '/05W5ibIDcNPkHXS8mC71CKB+10Ct9gzJ41C9HRMYeQ=');
+INSERT INTO public.merchants (id, name, secret) VALUES (default , 'Internal', 'C5X+rivjAkiczaDqkDwMKAVxN+cSGrudaGCRhWZeVos=');
+INSERT INTO public.merchants (id, name, secret) VALUES (default , 'Wayne Enterprise', '/05W5ibIDcNPkHXS8mC71CKB+10Ct9gzJ41C9HRMYeQ=');
 
-INSERT INTO public.accounts (id, active, currency, name, merchant_id) VALUES (1, true, 'USD', 'Cash account', 1);
-INSERT INTO public.accounts (id, active, currency, name, merchant_id) VALUES (2, true, 'USD', 'Card account', 1);
-INSERT INTO public.accounts (id, active, currency, name, merchant_id) VALUES (3, true, 'USD', 'Fee account', 1);
-INSERT INTO public.accounts (id, active, currency, name, merchant_id) VALUES (4, true, 'USD', 'Wayne USD account', 2);
+INSERT INTO public.accounts (id, active, currency, name, merchant_id) VALUES (default , true, 'USD', 'Cash account', 1);
+INSERT INTO public.accounts (id, active, currency, name, merchant_id) VALUES (default , true, 'USD', 'Card account', 1);
+INSERT INTO public.accounts (id, active, currency, name, merchant_id) VALUES (default , true, 'USD', 'Fee account', 1);
+INSERT INTO public.accounts (id, active, currency, name, merchant_id) VALUES (default , true, 'USD', 'Wayne USD account', 2);
